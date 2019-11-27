638a837b0e608f762b1ebaf08c00722725db3788
lucasrialb <32373554+lucasrialb@users.noreply.github.com>
2019-11-27 04:30:22 -0300

    Funciones terminadas (creo)

--- cache.c
+++ cache.c
@@ -1,7 +1,6 @@
+
 #include "cache.h"
 #include <string.h>
-
-#define MAX_COUNTER_VALUE 65535
 
 main_mem_t main_memory;
 
@@ -12,6 +11,13 @@ void init() {
 	memset(cache.blocks, 0, sizeof(cache.blocks));
 	cache.misses = 0;
 	cache.accesses = 0;
+
+	for(unsigned i = 0; i < CACHE_SETS; ++i){
+		for(unsigned j = 0; j < CACHE_WAYS; ++j) {
+			cache.blocks[i][j].valid = false;
+		}
+	}
+
 }
  
 unsigned int get_offset (unsigned int address) {
@@ -25,6 +31,7 @@ unsigned int find_set(unsigned int address) {
 unsigned int select_oldest(unsigned int setnum) {
 	uint16_t min = MAX_COUNTER_VALUE;
 	unsigned int way;
+
 	for(unsigned i = 0; i < CACHE_WAYS; ++i) {
 		uint16_t fifo_counter = cache.blocks[setnum][i].counter;
 		if (fifo_counter < min) {
@@ -35,24 +42,88 @@ unsigned int select_oldest(unsigned int setnum) {
 	return way;
 }
 
+void write_tomem (unsigned int blocknum, unsigned int way, unsigned int set){
+	unsigned char* mem_pos = &main_memory.main_mem[blocknum*CACHE_BLOCK_SIZE];
+	memcpy(mem_pos, cache.blocks[set][way].data, CACHE_BLOCK_SIZE);
+} 
+
 void read_tocache(unsigned int blocknum, unsigned int way, unsigned int set) {
 	unsigned char* mem_pos = &main_memory.main_mem[blocknum*CACHE_BLOCK_SIZE];
+	unsigned int tag = ((mem_pos & TAG_MASK) >> OFFSET_BITS)>> INDEX_BITS;
+	
+	if(cache.blocks[set][way].valid && cache.blocks[set][way].dirty){
+		write_tomem(blocknum, way, set);
+	}
 	memcpy(cache.blocks[set][way].data, mem_pos, CACHE_BLOCK_SIZE);
-
+	
 	cache.blocks[set][way].valid = true;
 	cache.blocks[set][way].dirty = false;
-	//TODO: tag
-	//TODO: fifo counter
+	cache.blocks[set][way].tag = tag;
+	cache.blocks[set][way].counter = 65535;
 }
 
 unsigned char read_byte(unsigned int address) {
-	return 0;
+	unsigned int offset = get_offset(address);
+	unsigned int set = find_set(address);
+	unsigned int tag = ((address & TAG_MASK) >> OFFSET_BITS)>> INDEX_BITS;
+	bool encontrado = false;
+	unsigned char resultado;
+	unsigned int way;
+
+	for(unsigned i = 0; i < CACHE_WAYS; ++i){
+		if(cache.blocks[set][i].valid && cache.blocks[set][i].tag == tag){
+			encontrado = true;
+		}
+	}
+	if(!encontrado){
+		way = select_oldest(set);
+		unsigned int blocknum = (address & MEMORY_INDEX_MASK) >> OFFSET_BITS;
+		read_tocache(blocknum, way, set);
+		cache.misses ++;
+	}
+	resultado = cache.blocks[set][i].data[offset]; 
+	cache.accesses ++;
+
+	return resultado;
 }
 
 void write_byte(unsigned int address, unsigned char value) {
 
+	//Busco a ver si esta en cache
+	//si no esta
+		//selecciono mas viejo
+		//veo si valid && dirty
+			//copio a memoria
+		//traigo bloque a cache
+	//escribo cache
+	//set dirty
+
+	unsigned int offset = get_offset(address);
+	unsigned int set = find_set(address);
+	unsigned int way;
+	unsigned int tag = ((address & TAG_MASK) >> OFFSET_BITS)>> INDEX_BITS;
+	bool encontrado = false;
+
+	for(unsigned i = 0; i < CACHE_WAYS; ++i){
+		if(cache.blocks[set][i].valid && cache.blocks[set][i].tag == tag){
+			encontrado = true;
+			way = i;
+		}
+	}
+	if(!encontrado){
+		way = select_oldest(set);
+		if(cache.blocks[set][way].valid && cache.blocks[set][way].dirty){
+			unsigned int blocknum = (address & MEMORY_INDEX_MASK) >> OFFSET_BITS;
+			write_tomem(blocknum, way, set);
+		}
+		read_tocache(blocknum, way, set);
+		cache.misses ++;
+	}
+	cache.blocks[set][way].data[offset] = value;
+	cache.blocks[set][way].dirty = true;
+	cache.accesses ++;
 }
 
 float get_miss_rate() {
-	return 0;
+	return cache.misses/cache.accesses;
 }
--- cache.h
+++ cache.h
@@ -1,13 +1,11 @@
+
 #ifndef CACHE_H
 #define CACHE_H
 
 #define _GNU_SOURCE 
 
-
 #include <stdint.h>
 #include <stdbool.h>
-
-#define MAIN_MEMORY_SIZE 65536
 
 /*
 *	CACHE
@@ -25,6 +23,7 @@
 *	-Metadata: Valid, Dirty, Tag, FIFO Counter.
 */
 
+#define MAIN_MEMORY_SIZE 65536
 #define CACHE_SIZE 16384
 #define CACHE_WAYS 8
 #define CACHE_SETS 32
@@ -34,8 +33,12 @@
 #define INDEX_BITS 5
 #define TAG_BITS 5
 
-#define OFFSET_MASK 0x3f
-#define INDEX_MASK 0x7c0
+#define OFFSET_MASK 0x003f
+#define INDEX_MASK 0x07c0
+#define TAG_MASK 0xf800
+#define MEMORY_INDEX_MASK 0xf7c0
+
+#define MAX_COUNTER_VALUE 65535
 
 typedef struct {
 	unsigned char main_mem[MAIN_MEMORY_SIZE];
@@ -62,6 +65,7 @@ unsigned int select_oldest(unsigned int setnum);
 void read_tocache(unsigned int blocknum, unsigned int way, unsigned int set);
 unsigned char read_byte(unsigned int address);
 void write_byte(unsigned int address, unsigned char value);
+void write_tomem (unsigned int blocknum, unsigned int way, unsigned int set);
 float get_miss_rate();
 
 #endif // CACHE_H
